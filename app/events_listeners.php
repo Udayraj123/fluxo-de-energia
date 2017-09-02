cb51ca7573434a36a85d44bfdb5e3093382a1558
Udayraj123 <rhuge123@gmail.com>
2017-09-02 08:45:25 +0530

    Auto Git

--- app/events_listeners.php
+++ app/events_listeners.php
@@ -1,18 +1,19 @@
 <?php
 Event::listen('bought_product',function($pch){
 	Game::userLog($pch->product->god_id,'Noted purchase : Notify the God, or Push to the table which is fetched with ajax');
+	Game::userLog($pch->product->farmer_id,'Noted purchase : Notify the Farmer of this purchase');
 	// Game::userLog($pch->god->addNotice(""));
 });
 
 Event::listen('bought_fruit',function($fbill){
 	Game::userLog($fbill->fruit->farmer_id,'Noted purchase : Notify the Farmer, or Push to the table which is fetched with ajax');
-	// Game::userLog($fbill->god->addNotice(""));
 });
 
 Event::listen('createProd',function($args){
 	$user = $args[0];
 	$p = $args[1];
 	$msg = ($p->created_at.' : '.$p->name.' is created in category of '.$p->category .' of quality '.$p->quality.' , the funding time for the product is '.$p->FT.' minutes and its expiry time is '.$p->ET.' minutes. The available quantity is '.$p->total_shares.' each prices at '.$p->unit_price.'. This Product is  created by '.$user->username.'. <br>'.'God says about his product :'.$p->description);
+	Game::userLog($user->id,$msg);
 	Event::fire('all_news',$msg);
 	// Game::userLog($pch->god->addNotice(""));
 });
